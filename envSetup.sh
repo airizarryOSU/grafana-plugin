d0f5aac189e2692cacfdf8730ec11410a704ca35
airizarryOSU <67487429+airizarryOSU@users.noreply.github.com>
2020-12-01 23:03:40 -0500

    Create envSetup.sh
    Environment setup file that handles the installation of all the packages and dependencies needed for creating a Grafana plugin.

---
+++ envSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yes | sudo yum install grafana
+
+yes sudo yum install git
+
+curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
+
+sudo yum install -y nodejs
+
+sudo npm i -g npx
+
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+
+yes | sudo yum install yarn
