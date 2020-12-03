654a53d3fc2623a3d440ad81c93c5d846c79836c
airizarryOSU <67487429+airizarryOSU@users.noreply.github.com>
2020-12-02 19:02:28 -0500

    Update envSetup.sh

--- envSetup.sh
+++ envSetup.sh
@@ -10,4 +10,4 @@ sudo npm i -g npx
 
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 
-yes | sudo yum install yarn
+yes | sudo yum install -y yarn
